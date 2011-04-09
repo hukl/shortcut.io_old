c3429abd4f9df1b98a904ea42b0a0d42cb7a1fed
hukl <contact@smyck.org>
2011-04-09 11:59:05 +0200

    changed home#index action to point to the current landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
 
-  before_filter :authenticate_account!
-
   def index
-    @urls = Url.where(
-      :account_id => current_account.id
-    ).order("created_at DESC")
+    render :layout => false
   end
-
+  
 end
