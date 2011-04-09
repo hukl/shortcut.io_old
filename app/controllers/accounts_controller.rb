de2a9e63e28b89054cccbf5067a50a9d8c1bc1c1
hukl <contact@smyck.org>
2011-04-10 00:08:50 +0200

    added vanilla account resource

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,14 @@
+class AccountsController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ test/functional/accounts_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class AccountsControllerTest < ActionController::TestCase
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get :destroy
+    assert_response :success
+  end
+
+end
