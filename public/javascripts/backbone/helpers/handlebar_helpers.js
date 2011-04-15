9069ad57216856ec287ae4d8888c324b42eb1359
hukl <contact@smyck.org>
2011-04-15 21:57:57 +0200

    thumb url refactoring

--- public/javascripts/backbone/helpers/handlebar_helpers.js
+++ public/javascripts/backbone/helpers/handlebar_helpers.js
@@ -2,23 +2,12 @@ Handlebars.registerHelper('truncate', function(title) {
   return title.substring(0, 30);
 });
 
-Handlebars.registerHelper('thumbnail_url_200', function(image_uuid) {
+Handlebars.registerHelper('thumbnail_url', function(image_uuid, size) {
   var segments = [
     "http://background.shortcut.io",
     image_uuid.substring(0,4),
     image_uuid.substring(4,8),
-    (image_uuid + "_200.jpg")
-  ];
-
-  return segments.join("/");
-});
-
-Handlebars.registerHelper('thumbnail_url_300', function(image_uuid) {
-  var segments = [
-    "http://background.shortcut.io",
-    image_uuid.substring(0,4),
-    image_uuid.substring(4,8),
-    (image_uuid + "_300.jpg")
+    (image_uuid + "_" + size + ".jpg")
   ];
 
   return segments.join("/");
