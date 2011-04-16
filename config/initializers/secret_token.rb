1ce12a6944fa96e45741f4d1c1a04da606dccb65
hukl <contact@smyck.org>
2011-04-17 01:43:04 +0200

    updated token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Frontend::Application.config.secret_token = '5dc50de89c53e3943f46b13343ec8eda0ace221de9156a8a3870399720230e625aee80d11af39ffab0b242601550bd5335ed98ee78ee13634d8efeb25548680f'
+Frontend::Application.config.secret_token = '5dc90de89c53e3943f46b13343ec8eda0ace221de9156a8a3870399720230e625aee80d11af39ffab0b242601550bd5335ed98ee78ee13634d8efeb25548680f'
