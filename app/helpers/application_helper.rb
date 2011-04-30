e6d408981b7c093ce188481a3ce09e1713a79cef
hukl <contact@smyck.org>
2011-04-30 17:34:52 +0200

    add nice helper for page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
-                     
+
   Tokens = %w(8ea5a734-629c-11e0-8969-27c4823e266e)
-  
-  def invited?               
+
+  def invited?
     Tokens.include? params[:invitation_token]
+  end
+
+  def title_for_page
+    if content_for?( :title )
+      content_for( :title )
+    else
+      params[:action]
+    end
   end
 end
