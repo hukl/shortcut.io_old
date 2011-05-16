1f33835feeef2d5dabdb8d23bf736a0508cc9f69
hukl <contact@smyck.org>
2011-05-16 11:15:54 +0200

    bookmarks importer now correctly imports the description as well and sanitizes broken html before parsing

--- app/models/bookmark_import.rb
+++ app/models/bookmark_import.rb
@@ -11,27 +11,42 @@ class BookmarkImport
 
     bookmarks = @@converter.iconv( bookmarks )
 
+    unless bookmarks =~ /\<\/DT\>/ && bookmarks =~ /\<\/DD\>/
+      bookmarks.gsub!(/\<DD\>(.*)$/) do |match|
+        "<DD>" + $1 + "</DD>"
+      end
+
+      bookmarks.gsub!(/\<\/A\>$/, "</A></DT>")
+      bookmarks.gsub!(/\<\/DL\>\<p\>/, "</DL></p>")
+    end
+
     doc = Nokogiri::HTML::Document.parse( bookmarks )
 
     Url.record_timestamps = false
 
-    doc.css("a").each do |node|
+    doc.css("dt").each do |node|
+      ahref = node.css("a").first
 
-      if node.attributes['add_date']
-        add_date = Time.at( node.attributes['add_date'].text.to_i )
+      if ahref.attributes['add_date']
+        add_date = Time.at( ahref.attributes['add_date'].text.to_i )
       else
         add_date = Time.now
       end
 
       options = {
-        :title      => node.text,
-        :uri        => node['href'],
+        :title      => ahref.text,
+        :uri        => ahref['href'],
         :created_at => add_date,
         :account_id => account_id
       }
 
+      if node.next_element.name == "dd"
+        options.merge!( :description => node.next_element.text )
+      end
+
+
       url =  Url.new( options )
-      url.tag_list = node.attributes['tags'].text if node.attributes['tags']
+      url.tag_list = ahref.attributes['tags'].text if ahref.attributes['tags']
 
       url.save
     end
