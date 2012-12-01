0363f594c67aea427a393f16c8b33427352f452d
hukl <contact@smyck.org>
2012-12-01 12:00:27 +0100

    simplified thumbnail_client for now

--- lib/thumbnail_client.rb
+++ lib/thumbnail_client.rb
@@ -1,23 +1,21 @@
 module ThumbnailClient
-  def intialize
-    @udp_socket ||= UDPSocket.new
-  end
-
-  def request(uri, image_uuid)
+  def self.request(uri, image_uuid)
     host  = "127.0.0.1"
     port  = 8789
+
+    puts($udp_socket.inspect)
 
     # <<UriLength:16/big,Uri:UriLength/binary,UudiLength:16/big,Uudi:UuidLength/binary>>
     packet = [uri.length, uri, image_uuid.length, image_uuid].pack(
       "nA*#{uri.length}nA*#{image_uuid.length}"
     )
 
-    @udp_socket.send( packet, 0, proxy_host, proxy_port )
-  rescue
-    if udp_socket.closed?
-      @udp_socket = UDPSocket.new
-    end
+    $udp_socket.send( packet, 0, host, port )
+  # rescue
+  #   if @udp_socket.closed?
+  #     @udp_socket = UDPSocket.new
+  #   end
 
-    false
+  #   false
   end
 end
